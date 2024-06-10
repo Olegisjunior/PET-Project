ca8945094a93269c2f4572ad4c47b4371f9e1de9
Olegisjunior <olegzavarinskiy@icloud.com>
2024-06-10 10:03:03 +0300

    Items to DB, and all logic reworked

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -4,7 +4,7 @@ export const Items = (props) => {
   const { name, img, price, id } = props;
 
   return (
-    <div className="w-[325px] h-[200px]  hover:shadow-[0_60px_30px_rgb(0,0,0,0.15)]">
+    <div className="w-[325px] h-[200px] hover:shadow-[0_60px_30px_rgb(0,0,0,0.15)]">
       <div className="bar flex justify-between">
         <a
           title="New Product"
@@ -25,7 +25,7 @@ export const Items = (props) => {
         />
       </a>
       <a className="justify-center hover:cursor-pointer flex wrap">{name}</a>
-      <a className="justify-center flex hover:cursor-pointer">{price}</a>
+      <a className="justify-center flex hover:cursor-pointer ">{`${price}$`}</a>
     </div>
   );
 };
